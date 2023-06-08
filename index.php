3e9d0c20ed10920b35fd14dedfa2b850bf0c1ca3
zomanya <135926710+zomanya@users.noreply.github.com>
2023-06-08 05:17:35 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,469 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['alogin']!=''){
+$_SESSION['alogin']='';
+}
+if(isset($_POST['login']))
+{
+$uname=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT Carrier,Password FROM staff WHERE Carrier=:uname and Password=:password";
+if ($uname=="chakula") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCM111") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'scm1dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCM222") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scm2dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="M/darasa1") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'darasa1dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCM444") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scm4dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCP111") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'scp1dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCP222") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scp2dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCP333") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scp3adashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCP444") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scp4dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCC111") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scc1adashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCC222") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scc2dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="SCC333") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'classes/scm3dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+
+elseif($uname=="master") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'master.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+elseif($uname=="account") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'codashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+
+
+
+elseif($uname=="malezi") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'discdashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+
+elseif($uname=="matron") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'hodashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+
+elseif($uname=="patron") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'hodashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+
+elseif ($uname=="admin") {
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+} else{
+    
+    echo "<script>alert('Invalid Details');</script>";
+
+}
+}
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login Panel</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
+        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
+        <link rel="stylesheet" href="css/prism/prism.css" media="screen" > <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+        <link rel="stylesheet" href="css/main.css" media="screen" >
+        <script src="js/modernizr/modernizr.min.js"></script>
+    </head>
+   <!--  <body class="">
+        <div class="main-wrapper">
+
+            <div class="">
+                <div class="row">
+ <h1 align="center">Student Result Management System</h1>
+ <h1 align="center">Haydom Institute Of Health Science</h1>
+                    <div class="col-lg-6 visible-lg-block">
+
+<section class="section">
+                            <div class="row mt-40">
+                                <div class="col-md-10 col-md-offset-1 pt-50">
+
+                                    <div class="row mt-30 ">
+                                        <div class="col-md-11">
+                                            <div class="panel">
+                                                <div class="panel-heading">
+                                                    <div class="panel-title text-center">
+                                                        <h4>For Students</h4>
+                                                    </div>
+                                                </div>
+                                                <div class="panel-body p-20">
+
+                                                    <div class="section-title">
+                                                        <p class="sub-title">Student Result Management System</p>
+                                                    </div>
+
+                                                    <form class="form-horizontal" method="post">
+                                                        <div class="form-group">
+                                                            <label for="inputEmail3" class="col-sm-6 control-label">Search your result</label>
+                                                            <div class="col-sm-6">
+                                                               <a href="find-result.php">click here</a>
+                                                            </div>
+                                                        </div>
+                                                       
+                                                    </form>
+
+                                            
+
+                                                 
+                                                </div>
+                                            </div>
+                                            <!-- /.panel -->
+                                      
+                                        </div>
+                                        <!-- /.col-md-11 -->
+                                    </div>
+                                    <!-- /.row -->
+                                </div>
+                                <!-- /.col-md-12 -->
+                            </div>
+                            <!-- /.row -->
+                        </section>
+                    </div>
+                    <div class="col-lg-6" style="margin-left: 350px">
+                        <section class="section">
+                            <div class="row mt-40">
+                                <div class="col-md-10 col-md-offset-1 pt-50">
+
+                                    <div class="row mt-30 ">
+                                        <div class="col-md-11">
+                                            <div class="panel">
+                                                <div class="panel-heading">
+                                                    <div class="panel-title text-center">
+                                                        <h4>Login Panel</h4>
+                                                    </div>
+                                                </div>
+                                                <div class="panel-body p-20">
+
+                                                    <div class="section-title">
+                                                        <p class="sub-title">JOFU</p>
+                                                    </div>
+
+                                                    <form class="form-horizontal" method="post">
+                                                        <div class="form-group">
+                                                            <label for="inputEmail3" class="col-sm-2 control-label">Username</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="text" name="username" class="form-control" id="inputEmail3" placeholder="UserName">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                                                            <div class="col-sm-10">
+                                                                <input type="password" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                                
+                                                        </div>
+                                                    
+                                                        <div class="form-group mt-20">
+                                                            <div class="col-sm-offset-2 col-sm-10">
+                                                           
+                                                                <button type="submit" name="login" class="btn btn-success btn-labeled pull-right">Sign in<span class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
+                                                            </div>
+                                                        </div>
+                                                    </form>
+
+                                            
+
+                                                 
+                                                </div>
+                                            </div>
+                                            <!-- /.panel -->
+                                            <p class="text-muted text-center"><small>&copy; 2020 Shule System</small></p>
+                                        </div>
+                                        <!-- /.col-md-11 -->
+                                    </div>
+                                    <!-- /.row -->
+                                </div>
+                                <!-- /.col-md-12 -->
+                            </div>
+                            <!-- /.row -->
+                        </section>
+
+                    </div>
+                    <!-- /.col-md-6 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /. -->
+
+        </div>
+        <!-- /.main-wrapper -->
+
+        <!-- ========== COMMON JS FILES ========== -->
+        <script src="js/jquery/jquery-2.2.4.min.js"></script>
+        <script src="js/jquery-ui/jquery-ui.min.js"></script>
+        <script src="js/bootstrap/bootstrap.min.js"></script>
+        <script src="js/pace/pace.min.js"></script>
+        <script src="js/lobipanel/lobipanel.min.js"></script>
+        <script src="js/iscroll/iscroll.js"></script>
+
+        <!-- ========== PAGE JS FILES ========== -->
+
+        <!-- ========== THEME JS ========== -->
+        <script src="js/main.js"></script>
+        <script>
+            $(function(){
+
+            });
+        </script>
+
+        <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
+    </body>
+</html>
